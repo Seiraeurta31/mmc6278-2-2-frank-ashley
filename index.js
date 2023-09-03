438b1ad9b1dd698680171b3d870d2dfd9e3aa21e
Ashley Frank <ashley.frank@ufl.edu>
2023-09-02 20:20:27 -0400

    added addQuote and getQuote functions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { program } = require("commander");
-const fs = require("fs/promises");
+const fs = require('fs/promises');
 const chalk = require("chalk");
-const { read } = require("fs");
+// const { read } = require("fs");
 const QUOTE_FILE = "quotes.txt";
 
 program
@@ -13,29 +13,18 @@ program
   .command("getQuote")
   .description("Retrieves a random quote")
   .action(async () => {
-    console.log("getQuote triggered")
-    
-    fs.readFile(QUOTE_FILE, 'utf-8', function(err, readData){
-      if(err) throw err
-      var array = readData.toString().split("\n") // load data from file into an array
-      for(i in array){
-        console.log(array[i]) // to validate data read in file
-      }
-      
-    })
-    
     // TODO: Pull a random quote from the quotes.txt file
     // console log the quote and author
     // You may style the text with chalk as you wish
-    
+
+    outputRandomQuote()
+      
   });
 
 program
   .command("addQuote <quote> [author]")
   .description("adds a quote to the quote file")
   .action(async (quote, author) => {
-
-    console.log("addQuote triggered")
     // TODO: Add the quote and author to the quotes.txt file
     // If no author is provided,
     // save the author as "Anonymous".
@@ -44,8 +33,42 @@ program
     // You may style the text with chalk as you wish
     // HINT: You can store both author and quote on the same line using
     // a separator like pipe | and then using .split() when retrieving
+
+    addQuoteToFile(quote, author)
+
   });
 
 program.parse();
 
 
+
+async function outputRandomQuote(){
+  try{
+    const readData = await fs.readFile(QUOTE_FILE, 'utf-8')
+    var dataAarray = readData.split("\n") //store lines of text in array
+    var randomQuoteLine = Math.floor(Math.random() * ((dataAarray.length -1)-0 +0)) 
+    var quoteArray = dataAarray[randomQuoteLine].split('|') //separate line into array per | separator
+    var quote = quoteArray[0]
+    var author = quoteArray[1]
+    console.log(chalk.bold('Quote: ') + chalk.italic('"' + quote + '"') + chalk.bold('\nAuthor: ') + chalk.italic(author))
+  }
+  catch{
+    console.log("Error reading file")
+  }
+     
+} 
+
+async function addQuoteToFile(quote, author){
+  try{
+    if(!author){ //if author is not provided, set to Anonymous
+      author = "Anonymous"
+    }
+    var newQuote = (quote + "|" + author) //combine & format quote and author into new line format
+    await fs.appendFile(QUOTE_FILE, newQuote + "\n")
+    console.log(chalk.bold('Quote: ') + chalk.italic('"' + quote + '"') + chalk.bold('\nAuthor: ') + chalk.italic(author))
+  }
+  catch{
+    console.log("Error adding quote")
+  }
+     
+} 
